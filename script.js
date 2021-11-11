c27521fb5b29e70c53fed3610a4ee241a2c2b8db
dhalladay <dave.halladay@outlook.com>
2021-11-10 17:05:19 -0700

    add generatePassword logic

--- script.js
+++ script.js
@@ -1,94 +1,90 @@
-//Assign generatePassword() function and add prompts
-var generatePassword = function() {
-  //create password variable to pass into html
-  var password = "";
-  //Ask if player wants to create password
-  var confirmPass = window.confirm("Generate a new password?")
-  if (confirmPass) {
-    //Create a variable that will contain all characters requested by user
-    var characterSet = function() {
-      var character = "";
-      var confirmAlpha = window.confirm("Include letters in your password?");
-      var confirmNum = window.confirm("Include numbers in your password?");
-      var confirmSym = window.confirm("Include symbols in your password?");
-      
-      if(confirmAlpha) {
-        var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-      } else {
-        var alpha = "";
-      };
-    
-      if(confirmNum) {
-        var numCon = "0123456789";
-      } else {
-        var numCon = "";
-      };
-      
-      if(confirmSym) {
-        var symbols = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-      } else {
-        var symbols = ""
-      };
-      character = (alpha + numCon + symbols)
-      return character;
-    }
-    
-    // character();
-    
-    //create variable for password length between 8 and 128 characters in lenght
-    var characterLength = function () {
-      var length = "";
-      
-      while (length === "") {
-        length = parseInt(window.prompt("How long would you like your password to be? Please select a number between 8-128", ""));
+//Create a variable that will contain all characters requested by user
+var characterSet = function() {
+  var character = "";
+  var confirmAlpha = window.confirm("Include letters in your password?");
+  var confirmNum = window.confirm("Include numbers in your password?");
+  var confirmSym = window.confirm("Include symbols in your password?");
+  
+  if(confirmAlpha) {
+    var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  } else {
+    var alpha = "";
+  };
+  
+  if(confirmNum) {
+    var numCon = "0123456789";
+  } else {
+    var numCon = "";
+  };
+  
+  if(confirmSym) {
+    var symbols = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else {
+    var symbols = ""
+  };
+  character = (alpha + numCon + symbols)
+  return character;
+}
+//create variable for password length between 8 and 128 characters in lenght
+var characterLength = function () {
+  var charLength = "";
+  
+      while (charLength === "") {
+        charLength = parseInt(window.prompt("How long would you like your password to be? Please select a number between 8-128", ""));
         
-        if (length >= 8 && length <= 128) {
-          return length;
+        if (charLength >= 8 && charLength <= 128) {
+          return charLength;
           break;
         }
-        else if (isNaN(length)) {
+        else if (isNaN(charLength)) {
           parseInt(window.alert("That is not a number. Please choose a number from 8-128."))
           return characterLength();
         }
-        else if (length < 8) {
-          parseInt(window.alert("Your number " + length + " is less than 8. Please choose a number between 8-128."))
+        else if (charLength < 8) {
+          parseInt(window.alert("Your number " + charLength + " is less than 8. Please choose a number between 8-128."))
           return characterLength();
         }
         else {
-          parseInt(window.prompt("Your number " + length + " is more than 128. Please choose a number between 8-128."))
+          parseInt(window.prompt("Your number " + charLength + " is more than 128. Please choose a number between 8-128."))
           return characterLength();
         }
       };
       
-      characterLength = length;
-      
-      // characterLength();
+      characterLength = charLength;
     }
-    
-    // Assignment Code
-    var generateBtn = document.querySelector("#generate");
-    
-    // Write password to the #password input
-    function writePassword() {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-      
-      passwordText.value = password;
-      
+
+  //Assign generatePassword() function and add prompts
+  var generatePassword = function() {
+    //Ask if player wants to create password
+    var confirmPass = window.confirm("Generate a new password?")
+    if (confirmPass) {
+      //create password variable to pass into html
+      var password = "";
+      //create other variables needed to generate password
+      var character = characterSet();
+      var charLength = characterLength();
+      for (var i = 0; i < charLength - 1; i++) {
+        password += character.charAt(Math.floor(Math.random() * charLength));
+      }
+      return password;
+      console.log(password);
     }
+    else {
+      window.alert("A password will not be created.")
+    }
+  };
+
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
     
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-  } 
-  else {
-    window.alert("A password will not be created.")
+    passwordText.value = password;
+    
   }
-  //create other variables needed to generate password
-  var character = characterSet();
-  var length = characterLength();
-  console.log(character);
-  console.log(length);
-};
-
-generatePassword();
-
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
